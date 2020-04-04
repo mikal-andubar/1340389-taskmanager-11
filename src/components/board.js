5faf3fb44d7fa99cfce9f7700c13dc15c7589e79
ifyodorov85 <mikal_andubar@mail.ru>
2020-04-04 19:59:04 +0300

    Добавил забытые новые файлы

---
+++ src/components/board.js
@@ -0,0 +1,9 @@
+/**
+ * Создание шаблона доски задач
+ * @return {string}
+ */
+export const createBoardTemplate = () => (
+  `<section class="board container">
+    <div class="board__tasks"></div>
+  </section>`
+);
